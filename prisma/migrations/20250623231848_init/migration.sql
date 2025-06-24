0f388f3f1e4f774f3afdd099996dc371ed0c9759
Jhonatan Silva Carvalho <jhonatan.silva.4198@gmail.com>
2025-06-24 08:52:47 -0400

    -

--- prisma/migrations/20250623231848_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Cliente" (
-    "id" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
-    "unidade" TEXT NOT NULL,
-    "subunidade" TEXT,
-
-    CONSTRAINT "Cliente_pkey" PRIMARY KEY ("id")
-);
