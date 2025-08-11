166c47fc8e9239cc75ff64a48cf7a29cb26c4fd2
Clara Helena <106986725+clarasenx@users.noreply.github.com>
2025-08-11 10:12:12 -0400

    [update]: cors

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,18 @@
 import Fastify from 'fastify';
 import mercurius from 'mercurius';
 import prismaPlugin from './plugins/prisma';
-import { buildSchema } from './schema/index'; // <-- importa a função agora
+import { buildSchema } from './schema/index';
+import cors from '@fastify/cors';
 
 export async function buildServer() {
   const app = Fastify();
 
   // await app.register(prismaPlugin);
 
+  app.register(cors, {
+    origin: '*', // ou pode ser um array ou função para controlar quais origens permitir
+  });
+  
   const schema = await buildSchema(app); // <-- chama a função com `app`
 
   app.register(mercurius, {
